a10b31df1367def66959dd27cb2cca7fcb954672
Stasa Trajkovic <trajkovicstasa04@gmail.com>
2025-12-16 02:30:03 +0100

    Vezbe4DrugaPolovina

--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,15 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:notes_hub/screens/cart_screen.dart';
+import 'package:notes_hub/screens/home_screen.dart';
+import 'package:notes_hub/screens/profile_screen.dart';
+import 'package:notes_hub/screens/search_screen.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class RootScreen extends StatefulWidget {
+  const RootScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<RootScreen> createState() => _RootScreenState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _RootScreenState extends State<RootScreen> {
+  late List<Widget> screens;
+  int currentScreen = 3;
+  late PageController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    screens = const [
+      HomeScreen(),
+      SearchScreen(),
+      CartScreen(),
+      ProfileScreen(),
+    ];
+    controller = PageController(initialPage: currentScreen);
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: screens,
+      ),
+
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentScreen,
+        height: kBottomNavigationBarHeight,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentScreen = index;
+          });
+
+          controller.jumpToPage(currentScreen);
+        },
+
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(IconlyBold.home),
+            icon: Icon(IconlyLight.home),
+            label: "Home",
+          ),
+
+          NavigationDestination(
+            selectedIcon: Icon(IconlyBold.search),
+            icon: Icon(IconlyLight.search),
+            label: "Search",
+          ),
+
+          NavigationDestination(
+            selectedIcon: Icon(IconlyBold.bag2),
+            icon: Icon(IconlyLight.bag2),
+            label:"Cart",
+          ),
+
+          NavigationDestination(  
+            selectedIcon: Icon(IconlyBold.profile),
+            icon: Icon(IconlyLight.profile),
+            label: "Profile",
+          )
+
+        ],
+      ),
+    );
   }
 }
